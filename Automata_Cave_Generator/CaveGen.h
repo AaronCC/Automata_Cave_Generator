de4c89bd2347fd1efee928279fac7a7c4f71724c
AaronCC <acc1414@rit.edu>
2018-04-04 19:33:14 -0400

    Cave generates with user input

--- Automata_Cave_Generator/CaveGen.h
+++ Automata_Cave_Generator/CaveGen.h
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <algorithm>
 #include <iostream>
 #include <random>
 
 class CaveGen {
 public:
-	CaveGen(int _map[], const int _width, const int _height) {
+	CaveGen(int _map[], const int _width, const int _height, int _dLimit, int _bLimit, int iterations) {
 		this->map = _map;
 		this->width = _width;
 		this->height = _height;
+		this->dLimit = _dLimit;
+		this->bLimit = _bLimit;
 		int liveChance = 40; // 40% chance to start open
 		for (int r = 0; r < height; r++)
 		{
@@ -20,6 +21,8 @@ public:
 					map[r*width + c] = 0;
 			}
 		}
+		for (int i = 0; i < iterations; i++)
+			iterate();
 		print();
 	}
 	void print() {
@@ -34,7 +37,8 @@ public:
 	}
 private:
 	int* map;
-	int width, height;
+	int* newMap;
+	int width, height, dLimit, bLimit;
 	int random() // return a value 1-100
 	{
 		std::random_device rd;
@@ -42,5 +46,50 @@ private:
 		std::uniform_int_distribution<> dist(0, 99);
 		return dist(gen) + 1;
 	}
-	char iToChar[2] = { '-' , '#' };
+	void iterate() {
+		const int size = width * height;
+		newMap = new int[size];
+		for (int c = 0; c < width; c++)
+		{
+			for (int r = 0; r < height; r++)
+			{
+				int nCount = getAliveNeighbors(c, r);
+				if ((bool)map[r*width + c]) {
+					if (nCount < dLimit)
+						newMap[r*width + c] = 0;
+					else
+						newMap[r*width + c] = 1;
+				}
+				else
+				{
+					if (nCount > bLimit)
+						newMap[r*width + c] = 0;
+					else
+						newMap[r*width + c] = 1;
+				}
+			}
+		}
+		std::memcpy(newMap, map, size * sizeof(int));
+	}
+	int getAliveNeighbors(int x, int y)
+	{
+		int count = 0;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				int c = x + i;
+				int r = y + j;
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				else if (c < 0 || r < 0 || c >= width || r >= height) {
+					count = count + 1;
+				}
+				else if ((bool)map[r*width + c]) {
+					count = count + 1;
+				}
+			}
+		}
+		return count;
+	}
+	char iToChar[2] = { ' ' , '#' };
 };
--- Automata_Cave_Generator/Main.cpp
+++ Automata_Cave_Generator/Main.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <iostream>
 #include <sstream>
 #include "CaveGen.h"
 
 int main()
 {
-	const int width = 25, height = 25;
-	int map[width * height];
-	CaveGen gen = CaveGen(map, width, height);
+	int death, birth, it, width, height;
+	int* map;
+	std::stringstream ss;
+	std::string in = "";
+	while (in != "q")
+	{
+		std::cout << "[q]uit\nEnter cave parameters (width height iterations deathLimit birthLimit): ";
+		std::getline(std::cin, in);
+		ss << in;
+		ss >> width >> height >> it >> death >> birth;
+		map = new int[width * height];
+		CaveGen gen = CaveGen(map, width, height, death, birth, it);
+		std::cout << "\n";
+	}
 	return 0;
 }
